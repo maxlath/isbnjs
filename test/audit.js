6e33fc6518ff414dde32c9d5868eb47e526353c2
maxlath <k@maxlath.eu>
2020-04-21 23:09:45 +0200

    audit: guess prefix from checksum

--- test/audit.js
+++ test/audit.js
@@ -2,21 +2,39 @@ const should = require('should')
 const { audit } = require('../isbn')
 
 describe('audit', () => {
-  it('should find potential 979-prefixed ISBN-13 converted to ISBN10 and wrongly reconverted to 978-prefixed ISBN13', () => {
+  it('should find 978-prefixed ISBN-13 that could be 979-prefixed ISBN-13 with altered checksum', () => {
     const clues = [
-      { label: 'possible prefix error', candidate: '979-10-90648-52-4' }
+      { message: 'possible prefix error', candidate: '979-10-90648-52-4' }
     ]
     audit('978-1-0906-4852-5').clues.should.deepEqual(clues)
     audit('9781090648525').clues.should.deepEqual(clues)
     audit('978-1090648525').clues.should.deepEqual(clues)
   })
 
-  it('should find 979-prefixed ISBN-13 passed as invalid 978-prefixed ISBN-13', () => {
+  it('should find 978-prefixed ISBN-13 that could be 979-prefixed ISBN-13 with altered checksum', () => {
     const clues = [
-      { label: 'wrong prefix', candidate: '979-10-90648-52-4' }
+      { message: 'possible prefix error', candidate: '978-1-0906-4852-5' }
+    ]
+    audit('979-10-90648-52-4').clues.should.deepEqual(clues)
+    audit('9791090648524').clues.should.deepEqual(clues)
+    audit('979-1090648524').clues.should.deepEqual(clues)
+  })
+
+  it('should find invalid 978-prefixed ISBN-13 that could be valid 979-prefixed ISBN-13', () => {
+    const clues = [
+      { message: 'checksum hints different prefix', candidate: '979-10-90648-52-4' }
     ]
     audit('978-1-0906-4852-4').clues.should.deepEqual(clues)
     audit('9781090648524').clues.should.deepEqual(clues)
     audit('978-1090648524').clues.should.deepEqual(clues)
+  })
+
+  it('should find invalid 979-prefixed ISBN-13 that could be valid 978-prefixed ISBN-13', () => {
+    const clues = [
+      { message: 'checksum hints different prefix', candidate: '978-1-0906-4852-5' }
+    ]
+    audit('979-10-906-4852-5').clues.should.deepEqual(clues)
+    audit('9791090648525').clues.should.deepEqual(clues)
+    audit('979-1090648525').clues.should.deepEqual(clues)
   })
 })
