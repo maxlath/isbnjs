547592350f9ae82b246faa01ffadbec765a78fa5
maxlath <k@maxlath.eu>
2020-04-21 22:10:26 +0200

    audit: find 979-prefixed ISBN-13 passed as invalid 978-prefixed ISBN-13

--- test/audit.js
+++ test/audit.js
@@ -6,8 +6,17 @@ describe('audit', () => {
     const clues = [
       { label: 'possible prefix error', candidate: '979-10-90648-52-4' }
     ]
-    audit('978-1-0906-4852-5').should.deepEqual({ source: '978-1-0906-4852-5', clues })
-    audit('9781090648525').should.deepEqual({ source: '9781090648525', clues })
-    audit('978-1090648525').should.deepEqual({ source: '978-1090648525', clues })
+    audit('978-1-0906-4852-5').clues.should.deepEqual(clues)
+    audit('9781090648525').clues.should.deepEqual(clues)
+    audit('978-1090648525').clues.should.deepEqual(clues)
+  })
+
+  it('should find 979-prefixed ISBN-13 passed as invalid 978-prefixed ISBN-13', () => {
+    const clues = [
+      { label: 'wrong prefix', candidate: '979-10-90648-52-4' }
+    ]
+    audit('978-1-0906-4852-4').clues.should.deepEqual(clues)
+    audit('9781090648524').clues.should.deepEqual(clues)
+    audit('978-1090648524').clues.should.deepEqual(clues)
   })
 })
